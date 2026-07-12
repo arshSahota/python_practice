da9ab094816e4e64aba9bde430f72d1150c8c5cc
Arshdeep Kaur <arshdeep_kaur@bcit.ca>
2026-07-11 21:24:42 -0700

    pytest fixtures practice

--- qa/temp.py
+++ qa/temp.py
@@ -5,23 +5,3 @@ def classify_temp(temp):
         return "HIGH_TEMP"
     else:
         return "LOW_TEMP"
-
-
-# mini exerise2: 
-
-# if the dev changes the function that way, then the boundary values are 
-# not included and then the boundary value 30 is included in the HIGH TEMP vals
-
-# 1. Which test fails
-# Ans: The 18, 30 
-# Ans2: 18 because its not included in the boundary values
-# Ans 3: code bug
-
-#mini exercise 4 function
-
-def check_alarm(sensor):
-    if sensor["temperature"] > 30:
-        return True
-    else:
-        return False
-
--- qa/test_temp.py
+++ qa/test_temp.py
@@ -1,66 +1,17 @@
 from temp import classify_temp
-from temp import check_alarm
+import pytest
 
-def test_temperature_below_minimum():
-    temp = 12
-    expected = "LOW_TEMP"
-    actual = classify_temp(temp)
-
-    assert actual == expected, (
-        f"Expected {expected} for temperature {temp}, but got {actual}"
-    )
-
-
-def test_temperature_at_minimum_boundary():
-    temp = 18
-    expected = "VALID"
-    actual = classify_temp(temp)
-
-    assert actual == expected, (
-        f"Expected {expected} for temperature {temp}, but got {actual}"
-    )
-
-def test_temperature_normal_valid():
-    temp = 22
-    expected = "VALID"
-    actual = classify_temp(temp)
-
-    assert actual == expected, (
-        f"Expected {expected} for temperature {temp}, but got {actual}"
-    )
-
-def test_temperature_at_maximum_boundary():
-    temp = 30
-    expected = "VALID"
-    actual = classify_temp(temp)
-
-    assert actual == expected, (
-        f"Expected {expected} for temperature {temp}, but got {actual}"
-    )
-
-def test_temperature_above_maximum():
-    temp = 35
-    expected = "HIGH_TEMP"
-    actual = classify_temp(temp)
-
-    assert actual == expected, (
-        f"Expected {expected} for temperature {temp}, but got {actual}"
-    )
-
-#mini exercise 3
-# def test_temperature_above_maximum():
-#     assert classify_temperature(31) == "HIGH_TEMP"
-
-#i think I have already done it above! 
-
-#mini exercise 4 test
-
-def test_check_alarm():
-    temp = {
-        "temperature": 30
+@pytest.fixture
+def high_temp_sensor():
+    return {
+        "room": "Mechanical Room",
+        "temperature": 35,
+        "alarm": True
     }
-    expected = False
-    actual = check_alarm(temp)
-    assert actual == expected, (
-        f"Expected {expected} for temperature {temp} but got {actual}"
-    )
+
+def test_sensor_has_high_temperature(high_temp_sensor):
+    assert high_temp_sensor["temperature"] > 30
+
+
+def test_sensor_alarm_is_on(high_temp_sensor):
+    assert high_temp_sensor["alarm"] == True
