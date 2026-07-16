330c83b714f482744b4cee5f119ac40c6f2af0ee
Arshdeep Kaur <arshdeep_kaur@bcit.ca>
2026-07-16 09:14:45 -0700

    python dictionary and sets

--- tech_questions/complete.py
+++ tech_questions/complete.py
@@ -1,17 +1,50 @@
-data = [
-    {"cpu": 50, "memory": 60},
-    {"cpu": 90},
-    {"memory": 40}
+sensors = [
+    {
+        "room": "Server Room",
+        "temperature": 35,
+        "alarm": True,
+        "equipment": ["UPS", "CoolingUnit"]
+    },
+    {
+        "room": "Office 101",
+        "temperature": 24,
+        "alarm": False,
+        "equipment": ["Thermostat"]
+    },
+    {
+        "room": "Storage Room",
+        "temperature": 15,
+        "alarm": False,
+        "equipment": ["Sensor"]
+    },
+    {
+        "room": "Electrical Room",
+        "temperature": 33,
+        "alarm": False,
+        "equipment": ["Generator", "UPS"]
+    }
 ]
 
-def get_complete_records(data):
+valid_rooms = set()
+invalid_rooms = {}
+equipment_found = set()
 
-    complete = []
+def analyse_sensors(sensors):
+    for reading in sensors:
+        for item in reading["equipment"]:
+            equipment_found.add(item)
+        if reading["temperature"] > 30 and reading["alarm"] is True:
+            valid_rooms.add(reading["room"])
+        elif reading["temperature"] <= 30 and reading["alarm"] is False:
+            valid_rooms.add(reading["room"])
+        elif reading["temperature"] <= 30 and reading ["alarm"] is True:
+            invalid_rooms[reading["room"]] = "Temperature below 30 but alarm was on"
+        else:
+            invalid_rooms[reading["room"]] = "Temperature above 30 but alarm was off"
+    return {
+        "valid_rooms": valid_rooms,
+        "invalid_rooms": invalid_rooms,
+        "equipment_found": equipment_found
+    }
 
-    for reading in data:
-        if "cpu" in reading and "memory" in reading:
-            complete.append(reading)
-
-    return complete
-
-print(get_complete_records(data))
+print(analyse_sensors(sensors))
